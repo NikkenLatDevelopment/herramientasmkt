144ce060479337943bff860f2177baec84ec2753
Camilo <camilo.callejas@icloud.com>
2024-03-26 09:34:58 -0500

    Organización Folleto de Negocio

--- resources/views/layouts/business-brochure.blade.php
+++ resources/views/layouts/business-brochure.blade.php
@@ -19,7 +19,7 @@
 
         <!-- Estilos -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+        @vite(['resources/sass/business-brochure.scss', 'resources/js/business-brochure.js'])
         <!-- Estilos -->
 
         <!-- Google tag (gtag.js) -->
