bfc97972e0b731d9adb490307fde92b67c0fb955
Camilo <camilo.callejas@icloud.com>
2024-03-26 19:50:27 -0500

    Micro sitio Cafecito Financiero

--- resources/views/livewire/financial-coffee/index.blade.php
+++ resources/views/livewire/financial-coffee/index.blade.php
@@ -73,7 +73,7 @@
                                         </div>
 
                                         <hr class="text-secondary">
-                                        <button type="submit" wire:loading.attr="disabled" class="btn btn-success rounded-4 py-3 w-100">Participar <i class="fa-solid fa-floppy-disk ms-1"></i> <span wire:loading><div class="spinner-border spinner-border-sm ms-1" role="status"><span class="visually-hidden">Loading...</span></div></span></button>
+                                        <button type="submit" wire:target="photo" wire:loading.attr="disabled" class="btn btn-success rounded-4 py-3 w-100">Participar <i class="fa-solid fa-floppy-disk ms-1"></i> <span wire:loading  wire:target="photo"><div class="spinner-border spinner-border-sm ms-1" role="status"><span class="visually-hidden">Loading...</span></div></span></button>
                                     </form>
                                 </div>
 
